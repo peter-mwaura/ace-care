423e955d7124eb6122d76aee49f7940b1ab6dca7
peter-mwaura <peterkaranjagitz@gmail.com>
2025-07-19 19:07:51 +0300

    feat(navbar): add basic navbar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar';
+
 function App() {
-    return <div>Ace-care</div>;
+    return (
+        <main>
+            <Navbar />
+        </main>
+    );
 }
 
 export default App;
